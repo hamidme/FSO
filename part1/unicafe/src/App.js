6c6eb3d52c067cd445fa4dc73ef02032e556133a
hamidme <hwaasi@gmail.com>
2022-08-04 18:44:30 +0400

    modified for 1.6: uncafe step 1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,8 +1,31 @@
-
+import { useState } from 'react'
 function App() {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const goodClicker = () => {
+    setGood(good + 1)
+  }
+
+  const [neutral, setNeutral] = useState(0)
+  const neutralClicker = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const [bad, setbad] = useState(0)
+  const badClicker = () => {
+    setbad(bad + 1)
+  }
+
   return (
     <div>
-      Baba Oloju ede.
+      <h1>Give Feedback</h1>
+      <button onClick={goodClicker}>good</button>
+      <button onClick={neutralClicker}>neutral</button>
+      <button onClick={badClicker}>bad</button>
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   );
 }
