796220488da059d23dafaf200537dcf4add310e7
hamidme <hwaasi@gmail.com>
2022-08-09 13:57:42 +0400

    modified for 2.4: Course information step9

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
-
 const Total = ({exercise}) => {
+  //Using for loop to calculate sum
+
   // let sum = 0
   // console.log(exercise.parts)
   // for(let i of exercise.parts){
   //   sum+=i.exercises
   //   }
   
+
+  //using array reduce to calculate sum
   const sum = exercise.parts.reduce((prevSum, part) => (prevSum + part.exercises), 0)
  
   return (
