4e7270d3b3f333689f1ddc6df6673aaa84f94c9d
hamidme <hwaasi@gmail.com>
2023-01-08 11:35:50 +0100

    adding the weather API

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -4,7 +4,10 @@ import { useState, useEffect } from "react";
 function App() {
   const [countryInfo, setCountryInfo] = useState('')
   const [showCountry, setShowCountry] = useState('')
-
+  const [oneCountryInfo, setOneCountryInfo] = useState('')
+  const [countryWeather, setCountryWeather] = useState('')
+  
+  
   useEffect(() => {
     axios
     .get('https://restcountries.com/v3.1/all')
@@ -13,8 +16,44 @@ function App() {
     })
   }, [])
 
+  const api_key = process.env.REACT_APP_API_KEY
+  
+  const city_name = `Saint John's`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${api_key}`
+
+  useEffect(() => {
+    
+    axios
+    .get(url)
+    .then(res => {
+      setCountryWeather(res.data)
+    })
+  }, [])
+
+
   const handleFilterCountry = function(event){
     setShowCountry(event.target.value)
+  }
+
+  const handleCountry = function(country){
+    //setCountryIndex(lat)
+    setOneCountryInfo(
+      <>
+        <h2>{country.name.common}</h2>
+        <p>Capital: {country.capital}<br/>
+        Area: {country.area}</p>
+        <div>
+          <h2>Languages:</h2>
+          <ul>{Object.values(country.languages).map((language, index) => <li key={index}>{language}</li> )}</ul>
+        </div>
+        <img alt={country.capital} src={country.flags.png} />
+  
+          <h2>Weather in {country.capital}</h2>
+          <p>Temperature {(countryWeather.main.temp - 273.15).toFixed(2)} Celsius</p>
+        
+      </>
+    )
+  
   }
   
   const getCountry = countryInfo && countryInfo.filter(country => 
@@ -34,20 +73,23 @@ function App() {
           getCountry.length > 1 ?
           getCountry && getCountry.map((country,index) => 
             <div key={index}>
-              <h3>{country.name.common} {(showCountry !== '')}</h3>
+              <h3>
+                {country.name.common}{'\u00A0'}
+                {(showCountry !== '') ? <button onClick={() => handleCountry(country)}>Show</button> : null }
+              </h3>              
             </div>) : 
-          getCountry && getCountry.map((country,index) => 
-          <div key={index}>
-            <h2>{country.name.common}</h2>
-            <p>Capital: {country.capital}<br/>
-            Area: {country.area}</p>
-            <div>
-              <h2>Languages:</h2>
-              <ul>{Object.values(country.languages).map((language, index) => <li key={index}>{language}</li> )}</ul>
-            </div>
-            <img alt={country.capital} src={country.flags.png} />
-          </div>)
+            getCountry && getCountry.map((country,index) => 
+            <div key={index}>
+              <h3>
+                {country.name.common}{'\u00A0'}
+                {(showCountry !== '') ? <button onClick={() => handleCountry(country)}>Show</button> : null }
+              </h3>              
+            </div>)
       }
+      <>
+      {oneCountryInfo}
+      </>
+      
     </div>
   );
 }
